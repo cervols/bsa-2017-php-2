65170b0566c1fc63d1316c26b853fa58ec9c932c
vagrant <vagrant@homestead>
2017-05-29 21:53:16 +0000

    Task1 fixed

--- src/task1.sql
+++ src/task1.sql
@@ -1,1 +1,7 @@
-select * from users where age > 25;
+select u.*
+from users u
+where u.age > 25 and u.id in (select b.user_id
+ from tickets t join
+	bookings b on b.ticket_id = t.id
+group by b.user_id
+having sum(price) > 400);
