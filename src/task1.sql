34da6315626adce31b1b86bf2bf123c226ff51bd
cervols <tania.zavediuk@gmail.com>
2017-05-30 15:11:13 +0300

    task1 & task6 are solved in another way (without subqueries)

--- src/task1.sql
+++ src/task1.sql
@@ -1,7 +1,15 @@
-select u.*
+/*select u.*
 from users u
 where u.age > 25 and u.id in (select b.user_id
  from tickets t join
 	bookings b on b.ticket_id = t.id
 group by b.user_id
 having sum(price) > 400);
+*/
+select u.*
+from users u join
+ bookings b on u.id = b.user_id join
+  tickets t on b.ticket_id = t.id
+where u.age > 25
+group by u.id
+having sum(t.price) > 400;
--- src/task6.sql
+++ src/task6.sql
@@ -1,5 +1,11 @@
-select first_name, last_name, age from users u join
+/*select first_name, last_name, age from users u join
 (select b.user_id from bookings b join tickets t
 ON t.id = b.ticket_id
 where t.title = 'Lagos') AS vyb
 ON u.id = vyb.user_id;
+*/
+select u.*
+from users u join
+ bookings b ON u.id = b.user_id join
+  tickets t ON b.ticket_id = t.id
+where t.title = 'Lagos';
